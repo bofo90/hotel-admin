2bc85831d58d45becb23d92bf72e6c8d1feb470f
bofo90 <f.agustin@gmail.com>
2021-08-27 11:19:24 +0200

    Create models of hotels and cities.

--- hotels/models.py
+++ hotels/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+	"""
+	Stores the cities where the hotels are with a simplified code.
+	"""
+	name = models.CharField(max_length = 200)
+	code = models.CharField(max_length = 50)
+
+	def __str__(self):
+		return self.name
+
+class Hotel(models.Model):
+	"""
+	Stores the hotel pointing to a city with name and code.
+	"""
+	city = models.ForeignKey(City, on_delete = models.CASCADE)
+	name = models.CharField(max_length = 200)
+	code = models.CharField(max_length = 50)
+
+	def __str__(self):
+		return self.name
