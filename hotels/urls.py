dd352d7ab5b0b6c1770fcaef7c00e7189d306518
bofo90 <f.agustin@gmail.com>
2021-08-27 15:30:14 +0200

    Create view to display the available cities and the hotels in each city

--- hotels/urls.py
+++ hotels/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'hotels'
 urlpatterns = [
-	path('', views.index, name = 'index'),
+	# path to enter the index of cities
+	path('', views.IndexView.as_view(), name = 'index'),
+	# path showing the hotels in the selected city
+	path('<str:city_name>/', views.results, name = 'result')
 ]
--- hotels/views.py
+++ hotels/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
+# from django.http import HttpResponse
+# from django.template import loader
 
-def index(request):
-	return HttpResponse("Here come the hotels app.")
+from .models import City, Hotel
 
+class IndexView(generic.ListView):
+	"""
+	Use of generic view to list all cities that are on the database and 
+	display it according to hotels/index.html
+	"""
+	template_name = 'hotels/index.html'
+	context_object_name = 'all_cities'
 
+	def get_queryset(self):
+		""" Return all the cities for display """
+		return City.objects.order_by('name')
+
+def results(request, city_name):
+	"""
+	Create view that displays the hotels in a city following hotels/results.html
+	"""
+	c = get_object_or_404(City, name = city_name)
+	hotels_in_city = Hotel.objects.filter(city__name = city_name)
+	context = {
+		'hotels_in_city' : hotels_in_city,
+		'city_name' : city_name,
+	}
+
+	return render(request, 'hotels/result.html', context)
